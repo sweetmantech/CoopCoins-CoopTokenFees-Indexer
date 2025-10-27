20de240f4e5c18ef2de7dc35def996d3e276d4ab
Sweets Sweetman <sweetmantech@gmail.com>
2025-10-27 14:12:22 -0500

    Indexer - CoopTokenFees

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,35 +1,24 @@
 /*
  * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
  */
-import {
-  LockableUniswapV3Initializer,
-  LockableUniswapV3Initializer_Lock,
-  BeneficiaryData,
-} from "generated";
+import { CoopCoin, CoopCoin_CoopTokenFees } from "generated";
 
-LockableUniswapV3Initializer.Lock.handler(async ({ event, context }) => {
-  const lockId = `${event.chainId}_${event.block.number}_${event.logIndex}`;
-
-  // Create the Lock entity
-  const lockEntity: LockableUniswapV3Initializer_Lock = {
-    id: lockId,
-    pool: event.params.pool,
-    transactionHash: event.transaction.hash,
-    blockNumber: event.block.number,
-  };
-  context.LockableUniswapV3Initializer_Lock.set(lockEntity);
-
-  // Process the beneficiaries tuple array
-  event.params.beneficiaries.forEach((beneficiary: any, index: number) => {
-    const beneficiaryEntity: BeneficiaryData = {
-      id: `${lockId}_${index}`,
-      lockId: lockId,
-      pool: event.params.pool,
-      beneficiary: beneficiary[0], // First element is beneficiary address
-      shares: beneficiary[1], // Second element is shares
-      transactionHash: event.transaction.hash,
-      blockNumber: event.block.number,
+CoopCoin.CoopTokenFees.handler(
+  async ({ event, context }) => {
+    const coopCoinId = `${event.chainId}_${event.block.number}_${event.logIndex}`;
+    const coopCoinEntity: CoopCoin_CoopTokenFees = {
+      id: coopCoinId,
+      tokenCreator: event.params.tokenCreator,
+      platformReferrer: event.params.platformReferrer,
+      orderReferrer: event.params.orderReferrer,
+      protocolFeeRecipient: event.params.protocolFeeRecipient,
+      tokenCreatorFee: event.params.tokenCreatorFee,
+      platformReferrerFee: event.params.platformReferrerFee,
+      orderReferrerFee: event.params.orderReferrerFee,
+      protocolFee: event.params.protocolFee,
+      contract: event.srcAddress,
     };
-    context.BeneficiaryData.set(beneficiaryEntity);
-  });
-});
+    context.CoopCoin_CoopTokenFees.set(coopCoinEntity);
+  },
+  { wildcard: true }
+);
